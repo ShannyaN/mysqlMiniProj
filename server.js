505b8286b532bd235de26325cd4d608b71396187
Shannya <niveyro101@gmail.com>
2023-03-13 14:32:49 -0400

    starting of routes

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const mysql = require('mysql2')
 
 //DATA
 
+//DB CONNECTION
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password:" ",
+    database:"movie_db"
+},
+console.log("db connected"))
 
 //APP/PORT
 const app = express();
@@ -14,6 +22,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }))
 
 //ROUTES
+// It's done when movie_db is created and contains a movies and reviews table.
+
+// It's done when movie_db has been seeded with data.
+
+// It's done when the /api/movies route renders a list of all movies.
+
+// It's done when the /api/add-movie route successfully adds a movie when tested using Insomnia.
+
+// It's done when the /api/update-review route successfully updates a movie when tested using Insomnia.
+
+// It's done when the /api/movie/:id route deletes a route when tested using Insomnia.
+
+app.get("*",(req,res)=>res.status(404).send("Nothing found yet"))
 
 //START THE SERVER(APP)
 app.listen(PORT,()=> console.log(`The server is running at https://localhost:${PORT}`))
