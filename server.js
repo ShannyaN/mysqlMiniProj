110530137e42e02e5d3399f19fe7dbb448488d0e
Shannya <niveyro101@gmail.com>
2023-03-13 20:55:09 -0400

    working on delete

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ console.log("db connected"))
 
 //APP/PORT
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 //MIDDLEWARE
 app.use(express.json());
@@ -26,18 +26,46 @@ app.use(express.urlencoded({ extended : true }))
 app.get('/api/movies',(req,res)=>{
     let movies;
     db.query(`SELECT * FROM movies`,(err, result) =>{
-        movies=result;
-        res.json(movies);
-        //res.json(result)--another way to do it
+        // movies=result;
+        // res.json(movies);
+        res.json(result)//another way to do it
     })
 })
+
+app.get('/api/movie/:id',(req,res)=>{
+    const iden = req.params.id;
+    //res.json(iden)
+    db.query(`SELECT * FROM movies WHERE id = ${iden};`,(err, result) =>{
+        res.json(result);
+    })
+    console.log("api found")
+})
+
 // It's done when the /api/add-movie route successfully adds a movie when tested using Insomnia.
+app.post('/api/add-movie',(req,res)=>{
+    const {movie} = req.body;
+    db.query(`INSERT INTO movies(movie_name) VALUES ('${movie}');`,(err, result) =>{
+        console.log(result + "added")})
+    db.query('SELECT * FROM movies', function (err, results) {
+       res.json(results);
+      });})
+
 
 // It's done when the /api/update-review route successfully updates a movie when tested using Insomnia.
 
 // It's done when the /api/movie/:id route deletes a route when tested using Insomnia.
+app.delete('/api/movie/:id',(req,res)=>{
+    const iden = req.params.id;
+    //res.json(iden)
+    db.query(`DELETE * FROM movies WHERE id = ${iden};`,(err, result) =>{
+        if (err){
+            res.json(err);
+        }
+        console.log("Deleted");
+    })
+})
 
 app.get("*",(req,res)=>res.status(404).send("Nothing found yet"))
 
 //START THE SERVER(APP)
-app.listen(PORT,()=> console.log(`The server is running at https://localhost:${PORT}`))
+app.listen(PORT,()=> console.log(`The server is running at http://localhost:${PORT}`))
